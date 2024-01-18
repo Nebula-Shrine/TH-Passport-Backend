aae13eeb4c8b154d801b2e910485cae656b52377
KohakuKirisame <kohaku@hepnovel.com>
2024-01-18 16:52:42 +0800

    Init

---
+++ src/main/java/com/nebulashrine/thpassport/ThPassportApplication.java
@@ -0,0 +1,13 @@
+package com.nebulashrine.thpassport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThPassportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThPassportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nebulashrine/thpassport/ThPassportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nebulashrine.thpassport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThPassportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
